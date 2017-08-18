a6fbf6c5b950a5834057c50b41f09060cddef717
Jason Collins <the.oem@gmail.com>
2017-08-18 10:26:22 -0600

    Heroku setup config.

---
+++ test/folders.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    name: "Memes",
+    description: "These are all my memes!",
+    id: 1
+  },
+  {
+    name: "Fantasy Football",
+    description: "These are all my football leagues!",
+    id: 2
+  },
+  {
+    name: "Random shit",
+    description: "These are all my random links!",
+    id: 3
+  }
+];
