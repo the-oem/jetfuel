b5a9986527b5d0fd753e110e87fcac7ed0c6cdc2
Jason Collins <the.oem@gmail.com>
2017-08-20 12:44:51 -0600

    Start on client refactoring and cleanup.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -13,8 +13,8 @@ let addLinkVisible = false;
 
 const pageSetup = () => {
   apiGetFolders()
-    .then(folders => folderArray = folders)
-    .then(folders => loadFolders(folderArray))
+    .then(response => folderArray = response.data)
+    .then(response => loadFolders(folderArray))
 }
 
 // Document setup
@@ -42,6 +42,8 @@ const loadFolders = (folders) => {
   loadFoldersInDom(sortedFolders);
 }
 
+
+
 const sortFolders = (folders) => {
   return folders.sort((a, b) => {
     return (a.name.toUpperCase() > b.name.toUpperCase()) ? 1 : ((b.name.toUpperCase() > a.name.toUpperCase()) ? -1 : 0);
@@ -50,29 +52,70 @@ const sortFolders = (folders) => {
 
 const buildFoldersInputSelect = (folders) => {
   var $selectInput = $('#inputLinkFolder').empty().html(' ');
-  $selectInput.append($(`<option value="">--- Select Parent Folder ---</option>`))
+  $selectInput.append($(`<option value="">Select Folder</option>`))
   for (let i = 0; i < folders.length; i++) {
     $selectInput.append($(`<option value="${folders[i].id}">${folders[i].name}</option>`))
   }
 }
 
+const addFolderInDom = (folder) => {
+  $('.folder-container').append(`
+    <div class="folder" id="${folder.id}">
+      <span class="folder-name">${folder.name}</span>
+      <span class="folder-description">${folder.description}</span>
+      <div class="folder-links" id="folderLinks"></div>
+    </div>
+  `)
+}
+
+const addLinkInDom = (folderId, link) => {
+  $(`#${folderId}`).find('.folder-links').append(`
+    <div class="link">
+      <a href="${link.short_url}">${link.url}</a>
+    </div>
+  `)
+}
+
 const loadFoldersInDom = (folders) => {
   $('.folder-container').empty();
   for (let i = 0; i < folders.length; i++) {
-    $('.folder-container').append(`<div class="folder" id="${folders[i].id}">Folder name: ${folders[i].name}, folder description: ${folders[i].description}</div>`)
+    addFolderInDom(folders[i]);
   }
+}
+
+const loadLinksInDom = (folderId, links) => {
+  console.log(folderId, links);
+  $(`#${folderId}`).find('.folder-links').empty();
+  if (links) {
+    for (let i = 0; i < links.length; i++) {
+      addLinkInDom(folderId, links[i]);
+    }
+  } else {
+    $(`#${folderId}`).find('.folder-links').append(`
+      <div class="link">
+        No saved links in this folder.
+      </div>
+    `)
+  }
+  // $(`#${folderId}`).find('.folder-links').toggle();
 }
 
 const addLink = () => {
   event.preventDefault();
-  const newLink = new Link($('.input-link-url').val(), $('.input-link-folder').val());
+  const newLink = new Link(
+    $('#inputLinkUrl').val(),
+    $('#inputLinkFolder').val()
+  );
   apiAddLink(newLink);
   $linkForm[0].reset();
 }
 
 const addFolder = () => {
   event.preventDefault();
-  const newFolder = new Folder($('#inputFolderName').val(), $('#inputFolderDesc').val());
+  const newFolder = new Folder(
+    $('#inputFolderName').val(),
+    $('#inputFolderDesc').val()
+  );
   apiAddFolder(newFolder);
   $folderForm[0].reset();
 }
@@ -87,14 +130,14 @@ const apiAddFolder = (folder) => {
   })
   .then(response => response.json())
   .then(response => {
-    folderArray.push(response.id);
+    folderArray.push(response.data);
+    // addFolderInDom(response.data);
     loadFolders(folderArray);
   })
   .catch(error => console.log(error));
 }
 
 const apiAddLink = (link) => {
-  // TODO Add link stuff here
   fetch("/api/v1/links", {
     method: 'POST',
     headers: {
@@ -103,17 +146,32 @@ const apiAddLink = (link) => {
     body: JSON.stringify(link),
   })
   .then(response => response.json())
+  .then(response => addLinkInDom(response.data.folder_id, response.data))
   .catch(error => console.log(error));
 }
 
 const apiGetFolders = () => {
   return fetch("/api/v1/folders")
   .then(response => response.json())
-  .catch(error => error);
+  .catch(error => console.log(error));
 }
 
 const apiGetFolderLinks = (folderId) => {
+  return fetch(`/api/v1/folders/${folderId}/links`)
+    .then(response => response.json())
+    .catch(error => console.log(error));
+}
 
+const toggleLinks = () => {
+  const currentFolder = $(event.target).parent();
+  const folderId = currentFolder.attr('id');
+  if(currentFolder.find('.folder-links').css('display') === 'none') {
+    apiGetFolderLinks(folderId)
+      .then(response => loadLinksInDom(folderId, response.data))
+    currentFolder.find('.folder-links').fadeIn(1000);
+  } else {
+    currentFolder.find('.folder-links').hide();
+  }
 }
 
 $addFolderBtn.on('click', () => {
@@ -121,22 +179,25 @@ $addFolderBtn.on('click', () => {
     addLinkVisible = false;
     $addLinkContainer.toggle(false);
     addFolderVisible = true;
-    $addFolderContainer.toggle(300);
-  } else {
-    addFolderVisible = false;
-    $addFolderContainer.toggle(300);
+    $addFolderContainer.fadeIn(500);
   }
+  // else {
+  //   addFolderVisible = false;
+  //   $addFolderContainer.fadeOut(500);
+  // }
 });
 $addLinkBtn.on('click', () => {
   if (!addLinkVisible) {
     addFolderVisible = false;
     $addFolderContainer.toggle(false);
     addLinkVisible = true;
-    $addLinkContainer.toggle(300);
-  } else {
-    addLinkVisible = false;
-    $addLinkContainer.toggle(300);
+    $addLinkContainer.fadeIn(300);
   }
+  // else {
+  //   addLinkVisible = false;
+  //   $addLinkContainer.fadeOut(300);
+  // }
 });
 $folderForm.on('submit', addFolder);
 $linkForm.on('submit', addLink);
+$('.folder-container').on('click', '.folder-name', toggleLinks);
