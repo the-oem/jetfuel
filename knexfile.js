dc8d485cbfb4032884416ada8898a12b43f3790c
Jason Collins <the.oem@gmail.com>
2017-08-21 09:47:50 -0600

    Add lint for CircleCI.

--- knexfile.js
+++ knexfile.js
@@ -1,34 +1,32 @@
-// Update with your config settings.
-
 module.exports = {
 
   development: {
     client: 'pg',
     connection: 'postgres://localhost/jetfuel',
     migrations: {
-      directory: __dirname + '/src/server/db/migrations',
+      directory: './src/server/db/migrations'
     },
     seeds: {
-      directory: __dirname + '/src/server/db/seeds/dev',
+      directory: './src/server/db/seeds/dev'
     },
-    useNullAsDefault: true,
+    useNullAsDefault: true
   },
   test: {
     client: 'pg',
     connection: process.env.DATABASE_URL || 'postgres://localhost/jetfueltest',
     useNullAsDefault: true,
     migrations: {
-      directory: __dirname + '/src/server/db/migrations'
+      directory: './src/server/db/migrations'
     },
     seeds: {
-      directory: __dirname + '/src/server/db/seeds/test'
+      directory: './src/server/db/seeds/test'
     }
   },
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
-      directory: __dirname + '/src/server/db/migrations'
+      directory: './src/server/db/migrations'
     },
     useNullAsDefault: true
   }
--- src/server/db/seeds/test/initial.js
+++ src/server/db/seeds/test/initial.js
@@ -5,16 +5,16 @@ exports.seed = function (knex, Promise) {
       return Promise.all([
         knex('folders').insert({
           name: 'Recipes',
-          description: 'My favorite recipes',
+          description: 'My favorite recipes'
         }, 'id')
-        .then((folder) => {
-          return knex('links').insert([
-            { folder_id: folder[0], url: 'http://www.recipe.com', short_url: 'asdf' },
-            { folder_id: folder[0], url: 'http://allrecipes.com', short_url: 'qwerty' },
-          ]);
-        })
-        .then(() => console.log('Seeding complete!'))
-        .catch(error => console.log(`Error seeding data: ${error}`)),
+          .then((folder) => {
+            return knex('links').insert([
+              { folder_id: folder[0], url: 'http://www.recipe.com', short_url: 'asdf' },
+              { folder_id: folder[0], url: 'http://allrecipes.com', short_url: 'qwerty' }
+            ]);
+          })
+          .then(() => console.log('Seeding complete!'))
+          .catch(error => console.log(`Error seeding data: ${error}`))
       ]);
     })
     .catch(error => console.log(`Error seeding data: ${error}`));
